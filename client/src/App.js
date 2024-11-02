aa9569ba97773c30a1eb117cb80b6c1ba157f6b5
Aaron Goldbin <agoldbin@madisoncollege.edu>
2024-11-02 02:52:34 -0500

    Create files for react app. Add files for worker process and communicating with express api.

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+// import React, {Component} from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/OtherPage">Other Page</Link>
+            <Route exact path="/" component={Fib} />
+            <Route path="/OtherPage" componet={OtherPage} />
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
